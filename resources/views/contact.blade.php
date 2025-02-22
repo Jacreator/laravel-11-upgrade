ca3f687323c72bf42ec966440c0e208c90075d24
James Adakole <jambone.james82@gmail.com>
2025-02-22 14:05:26 +0100

    added component nav-link and layout

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+  <x-slot:heading>
+    Contact page
+  </x-slot:heading>
+  <h1
+    class="mb-4 text-2xl font-bold"
+  >
+    Contact page
+  </h1>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+  <x-slot:heading>
+    Home page
+  </x-slot:heading>
+  <h1
+    class="mb-4 text-2xl font-bold"
+  >
+    Home
+  </h1>
+</x-layout>
